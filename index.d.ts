f94484d3d5454cb2d468800a3d327aaa33667ebc
Trotyl <trotyl@qq.com>
2016-07-19 17:57:51 +0800

    Add angular global functions.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,38 @@
-declare namespace angular {
+declare class Module {
+}
 
+declare class Injector {
+}
+
+declare class JQueryExtened {
+}
+
+declare namespace angular {
+    function bind<T extends Function>(self: any, fn: T, ...args: any[]): T
+    function bootstrap(element: Node, modules?: string[], config?: { strictDi: boolean }): Injector
+    function copy<T>(source: T, destination?: T): T
+    function element(element: string|Node): JQueryExtened
+    function equals<T>(o1: T, o2: T): boolean
+    function extend(dst: Object, ...src: Object[]): Object
+    function forEach(obj: Object, iterator: (value: any, key: string, obj: Object) => void, context?: any): Object
+    function forEach<T>(obj: T[], iterator: (value: T, key: number, obj: T[]) => void, context?: any): T[]
+    function fromJson(json: string): Object|any[]|string|number
+    function identity<T>(value: T): T
+    function injector(modules: (string|Function)[], strictDi?: boolean): Injector
+    function isArray(value: any): boolean
+    function isDate(value: any): boolean
+    function isDefined(value: any): boolean
+    function isElement(value: any): boolean
+    function isFunction(value: any): boolean
+    function isNumber(value: any): boolean
+    function isObject(value: any): boolean
+    function isString(value: any): boolean
+    function isUndefined(value: any): boolean
+    function merge(dst: Object, ...src: Object[]): Object
+    function module(name: string, requires?: string[], configFn?: Function): Module
+    function noop(): void
+    function reloadWithDebugInfo(): void
+    function toJson(obj: any, pretty: boolean|number): string
 }
 
 export = angular;
--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,17 @@
-import test = require('blue-tape');
+import test = require('blue-tape')
 
-import angular = require('angular');
+import angular = require('..')
+
+angular.bind({}, () => {})
+angular.bind({}, () => {}, 1)
+angular.bind({}, () => {}, 1, 2)
+
+angular.bootstrap(document, ['demo'])
+angular.bootstrap(document, ['demo'], { strictDi: true })
+
+let copy = angular.copy({})
+angular.copy({}, copy)
+
+angular.element('body')
+
+angular.equals(1, 2)
