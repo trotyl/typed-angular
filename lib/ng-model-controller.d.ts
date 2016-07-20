6ed542ccb34516c329a38f143efbe48ae7090b2a
Trotyl <trotyl@qq.com>
2016-07-20 23:29:53 +0800

    Add type definition for ngModel.NgModelController.

--- lib/ng-model-controller.d.ts
+++ lib/ng-model-controller.d.ts
@@ -1,3 +1,41 @@
-export interface NgModelController {
+import { MapObject, NoParameterFn } from './lang'
 
+export type SingleTranformFn = (value: any) => any
+export type ParserFn = SingleTranformFn
+export type FormatterFn = SingleTranformFn
+export type ValidatorFn = (modelValue: any, viewValue: any) => boolean
+export type AsyncValidatorFn = (modelValue: any, viewValue: any) => Promise<void>
+export type ViewChangeListener = NoParameterFn
+
+export interface NgModelController {
+    $viewValue: any
+    $modelValue: any
+    $parsers: ParserFn[]
+    $formatters: FormatterFn[]
+    $validators: MapObject<ValidatorFn>
+    $asyncValidators: MapObject<AsyncValidatorFn>
+    $viewChangeListeners: ViewChangeListener[]
+    // TODO: Check value type
+    $error: MapObject<void>
+    // TODO: Check value type
+    $pending: MapObject<void>
+    $untouched: boolean
+    $touched: boolean
+    $pristine: boolean
+    $dirty: boolean
+    $valid: boolean
+    $invalid: boolean
+    $name: string
+
+    $render(): void
+    $isEmpty(value: any): boolean
+    $setValidity(validationErrorKey: string, isValid: boolean): void
+    $setPristine(): void
+    $setDirty(): void
+    $setUntouched(): void
+    $setTouched(): void
+    $rollbackViewValue(): void
+    $validate(): void
+    $commitViewValue(): void
+    $setViewValue(value: any, trigger: string): void
 }
