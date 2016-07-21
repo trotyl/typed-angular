3a2d3807add9dfaa77c21ee9907ab1c590c3940f
Trotyl <trotyl@qq.com>
2016-07-21 10:06:02 +0800

    Add type definition for AnimateProvider.

--- lib/animate-provider.d.ts
+++ lib/animate-provider.d.ts
@@ -1,11 +1,20 @@
-import { InjectableFn } from './lang'
+import { InjectableFn, NoParameterFn } from './lang'
 
 export type FactoryFn<T> = InjectableFn<T>
+export type DoneFunction = NoParameterFn
 
 export interface Animation {
-
+    // TODO: check element type
+    setClass?(element, addedClasses, removedClasses, doneFunction: DoneFunction, options): void
+    addClass?(element, addedClasses, doneFunction: DoneFunction, options): void
+    removeClass?(element, removedClasses, doneFunction: DoneFunction, options): void
+    enter?(element, doneFunction: DoneFunction, options): void
+    leave?(element, doneFunction: DoneFunction, options): void
+    move?(element, doneFunction: DoneFunction, options): void
+    animate?(element, fromStyles, toStyles, doneFunction: DoneFunction, options): void
 }
 
 export interface AnimateProvider {
-    register<T extends Animation>(name: string, factory: FactoryFn<T>)
+    register<T extends Animation>(name: string, factory: FactoryFn<T>): void
+    classNameFilter(expression?: RegExp): RegExp
 }
